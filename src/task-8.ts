6bea3eb73ecc9563a2df79779e10dfd811da344c
Marin <gwstudiogw48090@gmail.com>
2026-02-22 21:05:54 +0200

    7 tasks done

---
+++ src/task-8.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+interface Post
+async function fetchPosts() {
+  const response = await axios.get(
+    "https://jsonplaceholder.typicode.com/posts",
+  );
+  return response.data;
+}
+
+fetchPosts().then((posts) => {
+  console.log(posts[0].title);
+});
